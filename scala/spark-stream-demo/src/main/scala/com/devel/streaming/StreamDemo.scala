ddacf2f65a2d96868cb959cdb4f477eb154108bb
胡正军2 <huzj2@cmschina.com.cn>
2019-12-28 19:11:47 +0800

    update

--- scala/spark-stream-demo/src/main/scala/com/devel/streaming/StreamDemo.scala
+++ scala/spark-stream-demo/src/main/scala/com/devel/streaming/StreamDemo.scala
@@ -13,7 +13,11 @@ object StreamDemo {
 
   def main(args: Array[String]){
 
-    val conf = new SparkConf().setAppName("wordcount")
+    val conf = new SparkConf()//.setAppName("stream-demo")
+    conf.set("spark.app.name", "stream-demo")
+    conf.set("spark.master", "local[4]")
+    conf.set("spark.ui.port", "36000")
+    
     val sc = new StreamingContext(conf, Seconds(1))
 
     val lines = sc.socketTextStream("hadp-dev", 7777)
