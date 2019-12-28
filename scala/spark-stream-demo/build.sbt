f445dce2674f61367f9762766480b42068b569c5
胡正军2 <huzj2@cmschina.com.cn>
2019-12-28 18:24:34 +0800

    update

--- scala/spark-stream-demo/build.sbt
+++ scala/spark-stream-demo/build.sbt
@@ -1,4 +1,4 @@
-name := "learning-spark-mini-example"
+name := "spark-straming-example"
 
 version := "1.0"
 
@@ -6,5 +6,6 @@ scalaVersion := "2.11.12"
 
 //libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
 libraryDependencies ++= Seq(
+	"org.apache.spark" %% "spark-core" % "2.4.4" % "provided",
 	"org.apache.spark" %% "spark-streaming" % "2.4.4" % "provided"
 )
---
+++ scala/spark-stream-demo/src/main/scala/com/devel/streaming/StreamDemo.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object StreamDemo {
+
+  def main(args: Array[String]){
+
+    val conf = new SparkConf().setAppName("wordcount")
+    val sc = new StreamingContext(conf, Seconds(1))
+
+    val lines = sc.socketTextStream("localhost", 7777)
+    val errorLines = lines.filter(_.contains("error"))
+    errorLines.print()
+
+  }
+}
